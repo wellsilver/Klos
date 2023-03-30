5f39b5b1f04d829bd434c8df3b31a21b57a2be84
wellsilver <wellsilver@gmail.com>
2023-03-30 19:04:40 -0400

    64 bit real mode

--- src/boot.asm
+++ src/boot.asm
@@ -1,29 +1,15 @@
 bits 16
 org 0x7C00
 
-helloworld: db "Hello World!",10,0
-
-mov bp, $
-mov si, helloworld
-call print_str
-jmp bp
-
-print_str:
-    mov ah,0x0E ; register prep for bios
-    mov bh,0x00
-    mov bl,0x07
-print:
-    mov al, [si] ; get char from pointer
-    inc si ; increase pointer
-
-    or al,al ; if end of string 0
-    jz return
-
-    int 0x10
-
-    jmp print ; move to next character
-return:
-    ret
+bits 64
+[SECTION .text64]
+MODE64_ENTRY:
+    mov ax, 0x18
+    mov ds, ax ; Set up data segment
+    mov ax, 0x20
+    mov ss, ax ; Set up stack segment
+    mov rsp, 0x20 ; Set stack pointer
+    mov rbp, rsp ; change pointer to 64 bit version
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
