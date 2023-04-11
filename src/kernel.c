1a53f45c755ea1ad2da4b055b8cc2e20938bb11d
wellsilver <wellsilver2016@gmail.com>
2023-04-10 20:40:41 -0400

    fix

--- src/kernel.c
+++ src/kernel.c
@@ -1,7 +1,7 @@
 extern void kernel() {
     unsigned char *VGA = (unsigned char *) 0xb8000;
     char msg[] = "Hello world!";
-    for (int loop=0;loop<30;loop++) {
+    for (int loop=0;loop<13;loop++) {
         VGA[loop*2] = msg[loop];
         VGA[(loop*2)+1] = 0x0f;
     }
