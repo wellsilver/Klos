646a61fa5eddec81c43f6ba58ce1e01046ea23f7
wellsilver <wellsilver2016@gmail.com>
2023-08-31 21:25:25 -0400

    hello world

--- src/boot.asm
+++ src/boot.asm
@@ -2,6 +2,7 @@
 
 ; file truncated to 3 sectors for kfs
 bits 16
+org 0x7C00
 
 jmp code
 nop
@@ -10,4 +11,29 @@ db "kfs"
 db 0
 
 code:
+  mov ah, 00h
+  mov al, 02h
+  int 10h
+  mov bx, string-1
+  mov ah, 0xE
+.strl:
+  inc bx
+  mov al, byte [bx]
+  cmp byte [bx], 0
+  jz .loop
+  int 10h
+  jmp .strl
+  
+.loop:
+  hlt
+  jmp .loop
 
+
+string: db "bootloader",0
+times 510-($-$$) db 0
+db 0x55
+db 0xAA
+
+
+
+times 1024-($-$$) db 0
