20b39afe8db46d25aaae28cf707328f41bed3a88
wellsilver <wellsilver2016@gmail.com>
2023-11-16 21:25:26 -0500

    print memory given

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,6 +1,9 @@
 asm(".org 0x00010000");
 
+unsigned char *vgacursor;
+
 void _start() {
+  vgacursor = (unsigned char *) 0xb8000;
   asm("jmp kernel");
 }
 
@@ -11,17 +14,25 @@ void _start() {
 #include "util/str.h"
 
 void kernel() {
-  unsigned char *vgacursor = (unsigned char *) 0xb8000;
-
   uint16_t mmaplen = *(uint16_t *) 0x7b0d;
   struct e820_entry *mmap = (struct e820_entry *) 0x7b0f;
+  int loop;
 
-  char buf[8];
+  int freemem = 0;
+  for (loop=0;loop<mmaplen;loop++) {
+    if (mmap[loop].type == 1) {
+      freemem += mmap[loop].length;
+    }
+  }
+  char buf[24];
+  for (loop=0;loop<24;loop++) {
+    buf[loop] = 0;
+  }
+  itoa(freemem, buf, 24);
   
-  itoa(mmap[0].base, buf, 8);
-
-  for (int loop=0;buf[loop]!=0;loop++) {
-    vgacursor[loop] = buf[loop];
+  for (loop=0;buf[loop/2]!=0;loop+=2) {
+    vgacursor[loop] = buf[loop/2];
+    vgacursor[loop+1] = 7;
   }
 
   while (1) asm("hlt");
