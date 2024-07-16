060e2d422b5debd1f1e70fa464e5574bbe37e7cd
wellsilver <wellsilver2016@gmail.com>
2024-07-15 23:55:07 -0400

    add ``apt update`` to scripts

--- bintil_cross.sh
+++ bintil_cross.sh
@@ -3,6 +3,7 @@ cd /tmp/bintilbuild
 wget https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.gz
 tar -xf binutils-2.41.tar.gz
 # below breaks alot, should reinstall it
+sudo apt -y update
 sudo apt -y remove build-essential bison flex libgmp-dev libmpc-dev libmpfr-dev texinfo libisl-dev
 sudo apt -y install build-essential bison flex libgmp-dev libmpc-dev libmpfr-dev texinfo libisl-dev
 # just realized all distributions dont have apt 🤦‍♂️
