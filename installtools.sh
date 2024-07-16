f430361100bf0b95d6544fd2ffe1c42355322eae
wellsilver <wellsilver2016@gmail.com>
2024-07-16 00:47:40 -0400

    I missed more stuff

--- installtools.sh
+++ installtools.sh
@@ -1,6 +1,6 @@
 # installs everything required using apt
-apt update
-apt install nasm qemu-system-x86 make truncate wget
+apt -y update
+apt -u install nasm qemu-system-x86 make wget
 bash ./bintil_cross.sh
 bash ./gcc_cross.sh
 bash ./limine_inst.sh
