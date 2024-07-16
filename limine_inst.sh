fdd4eb61fa34a4c3328b1ac8fab1bc05c49e1e3e
wellsilver <wellsilver2016@gmail.com>
2024-07-16 00:21:08 -0400

    limine_inst.sh

---
+++ limine_inst.sh
@@ -0,0 +1,6 @@
+mkdir /tmp/liminebuild
+cd /tmp/liminebuild
+# update git link with latest limine version
+git clone https://github.com/limine-bootloader/limine.git --branch=v7.x-binary .
+make
+sudo make install
