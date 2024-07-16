e49186d4f5973a5aa230cba55589bfd6e2840a67
wellsilver <wellsilver2016@gmail.com>
2024-07-16 00:52:03 -0400

    -y instead of -u

--- installtools.sh
+++ installtools.sh
@@ -1,6 +1,6 @@
 # installs everything required using apt
 apt -y update
-apt -y install nasm qemu-system-x86 make wget
+apt -y install nasm qemu-system-x86 make wget python3
 bash ./bintil_cross.sh
 bash ./gcc_cross.sh
 bash ./limine_inst.sh
