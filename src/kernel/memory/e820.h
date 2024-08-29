dc0b30555bb141aab89551e64941b5e40402d198
wellsilver <wellsilver2016@gmail.com>
2024-08-29 19:36:43 -0400

    funny

--- src/kernel/memory/e820.h
+++ src/kernel/memory/e820.h
@@ -6,7 +6,7 @@
 struct e820_s {
   uint64_t base;
   uint64_t length;
-  uint32_t type;
+  uint32_t type; // 1=usable
 };
 
 int memoryinit_e820_size() {
@@ -22,7 +22,7 @@ struct memory_map *memoryinit_e820(struct memory_map *map) {
   uint64_t allocatedbytes=0;
 
   for (int loop=0;loop<map_size;loop++) {
-    if (biosmap->type == 1) {
+    if (biosmap[loop].type == 1) {
       freebytes += biosmap->length;
     } else {
       allocatedbytes += biosmap->length;
