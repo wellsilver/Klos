25d6582f559177faf2794b0c0b6f385beef0a4fa
wellsilver <wellsilver2016@gmail.com>
2024-09-12 18:19:19 -0400

    remove limine_inst.sh

--- installtools.sh
+++ installtools.sh
@@ -3,4 +3,3 @@ apt -y update
 apt -y install nasm qemu-system-x86 make wget python3 parted
 bash ./bintil_cross.sh
 bash ./gcc_cross.sh
-bash ./limine_inst.sh
--- limine_inst.sh
+++
@@ -1,6 +0,0 @@
-mkdir /tmp/liminebuild
-cd /tmp/liminebuild
-# update git link with latest limine version
-git clone https://github.com/limine-bootloader/limine.git --branch=v7.x-binary .
-make
-make install
