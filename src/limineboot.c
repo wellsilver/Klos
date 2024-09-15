3c86d9b9ff162413b0d4f943109a8499c02dfc2f
wellsilver <wellsilver2016@gmail.com>
2024-09-15 00:25:09 -0400

    gete largest memory entry

--- src/limineboot.c
+++ src/limineboot.c
@@ -1,6 +1,7 @@
 // find klos and boot it
 #include "int.h"
 #include <limine.h>
+#define NULL 0
 
 __attribute__((used, section(".requests")))
 static volatile LIMINE_BASE_REVISION(2);
@@ -10,9 +11,17 @@ static volatile LIMINE_BASE_REVISION(2);
 // be made volatile or equivalent, _and_ they should be accessed at least
 // once or marked as used with the "used" attribute as done here.
 
+/*
 __attribute__((used, section(".requests")))
 static volatile struct limine_framebuffer_request framebuffer_request = {
   .id = LIMINE_FRAMEBUFFER_REQUEST,
+  .revision = 0
+};
+*/
+
+__attribute__((used, section(".requests")))
+static volatile struct limine_memmap_request memmap_request = {
+  .id = LIMINE_MEMMAP_REQUEST,
   .revision = 0
 };
 
@@ -33,7 +42,25 @@ void kmain(void) {
   // Ensure the bootloader actually understands our base revision (see spec).
   if (LIMINE_BASE_REVISION_SUPPORTED == 0)
     return;
+  
+  struct limine_memmap_entry largestfree;
+  largestfree.base = 0;
+  largestfree.length = 0;
+  uint64_t highest;
+  uint64_t numpages;
+
+  if (memmap_request.response != NULL) {
+    for (int loop=0;loop<memmap_request.response->entry_count;loop++) {
+      struct limine_memmap_entry *i = memmap_request.response->entries[loop];
+      if (i->type == LIMINE_MEMMAP_USABLE) {
+        if (i->length > largestfree.length) {
+          largestfree = *i;
+        }
+      }
+    }
+  }
 
 
   while (1) asm("hlt");
+  return;
 }
