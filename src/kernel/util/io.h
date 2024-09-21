a833cec9d58d29d0bcfa0e765644a4b794e43137
wellsilver <wellsilver2016@gmail.com>
2024-09-21 14:19:26 -0400

    inw and outw

--- src/kernel/util/io.h
+++ src/kernel/util/io.h
@@ -11,7 +11,18 @@ uint8_t inb(uint16_t port) {
 }
 
 void outb(uint16_t port, uint8_t a) {
-  asm("outb %1, %0 " :: "a"(a), "d"(port));
+  asm("outb %1, %0" :: "a"(a), "d"(port));
+}
+
+uint16_t inw(uint16_t port) {
+  uint16_t out;
+  asm("inw %0, %1": "=a"(out) : "d"(port));
+  
+  return out;
+}
+
+void outw(uint16_t port, uint16_t a) {
+  asm("outw %1, %0 " :: "a"(a), "d"(port));
 }
 
 #endif
