048850d237c9f162b442119a0121a59d4b710a6d
wellsilver <wellsilver2016@gmail.com>
2024-09-27 15:44:14 -0400

    minor

--- src/kernel/disc/atapio.h
+++ src/kernel/disc/atapio.h
@@ -10,16 +10,16 @@ void atapio_read(uint drive, uint64_t lba, uint sectors, uint16_t *ptr) {
   if (b == 0xFF) return; // error?
   if (b == 0x80) return;
 
-  outb(0x1F6, 0x40);
-  outb(0x1F1, 0x00);  // write 0x00 to the FEATURES register
-  outb(0x1F2, 0x01);  // write 0x01 to the Sector Count register
-  outb(0x1F3, (unsigned char) lba);  // Sector number or LBA Low, most likely LBA Low (but see comments below)
-  outb(0x1F4, (unsigned char)(lba >> 8));  // Cyl Low number or LBA Mid
-  outb(0x1F5, (unsigned char)(lba >> 16));  // Cyl High number or LBA High
-  outb(0x1F7, 0x20);  // Send command, See note [2] below
+  outb(0x1F6, 0x40); // master
+  outb(0x1F1, 0x00); // write 0x00 to the FEATURES register
+  outb(0x1F2, 0x01); // write 0x01 to the Sector Count register
+  outb(0x1F3, (unsigned char) lba); // Sector number or LBA Low, most likely LBA Low (but see comments below)
+  outb(0x1F4, (unsigned char)(lba >> 8)); // Cyl Low number or LBA Mid
+  outb(0x1F5, (unsigned char)(lba >> 16)); // Cyl High number or LBA High
+  outb(0x1F7, 0x20); // Send command, See note [2] below
 
   while (inb(0x1F7) == 80); // wait until ready
-
+  
   for (uint loop=0;loop<256;loop++) {
     ptr[loop] = inw(0x1F0);
   }
