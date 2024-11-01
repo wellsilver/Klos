ffd78fcef7358fc7110675c29c388558616045ef
wellsilver <wellsilver2016@gmail.com>
2024-10-31 20:31:52 -0400

    gdt

--- src/limineboot.c
+++ src/limineboot.c
@@ -39,7 +39,7 @@ static volatile LIMINE_REQUESTS_END_MARKER;
 
 // translated from src/boot.x86.S
 // a new gdt with all of memory listed as readable, writable, and executable
-asm(R"
+/*
 ; Access bits
 PRESENT        equ 1 << 7
 NOT_SYS        equ 1 << 4
@@ -52,7 +52,7 @@ ACCESSED       equ 1 << 0
 GRAN_4K       equ 1 << 7
 SZ_32         equ 1 << 6
 LONG_MODE     equ 1 << 5
-
+ 
 GDT64: ; 64 bit gdt
   .Null: equ $ - GDT64
     dq 0
@@ -74,8 +74,50 @@ GDT64: ; 64 bit gdt
   .Pointer:
     dw $ - GDT64 - 1
     dq GDT64
-");
-extern void *gdt64;
+*/
+struct _gdt64 {
+  uint64_t d0;
+  uint32_t d1;
+  uint8_t d2;
+  uint8_t d3;
+  uint8_t d4;
+  uint8_t d5;
+  uint32_t d6;
+  uint8_t d7;
+  uint8_t d8;
+  uint8_t d9;
+  uint8_t dA;
+  uint32_t dB;
+  uint32_t dC;
+} __attribute__((packed));
+struct _gdt64ptr {
+  uint16_t size;
+  void *ptr;
+} __attribute__((packed));
+
+// incase your wondering, yes I hate this
+__attribute__((used, section(".text")))
+static volatile struct _gdt64 gdt = {
+  .d0 = 0,
+  .d1 = 0xFFFF,
+  .d2 = 0,
+  .d3 = 1 << 7 | 1 << 4 | 1 << 3 | 1 << 1,
+  .d4 = 1 << 7 | 1 << 5 | 0xF,
+  .d5 = 0,
+  .d6 = 0xFFFF,
+  .d7 = 0,
+  .d8 = 1 << 7 | 1 << 4 | 1 << 1,
+  .d9 = 1 << 7 | 1 << 6 | 0xF,
+  .dA = 0,
+  .dB = 0x00000068,
+  .dC = 0x00CF8900
+};
+__attribute__((used, section(".text")))
+static volatile struct _gdt64ptr gdtptr = {
+  .size = sizeof(struct _gdt64) - 1,
+  .ptr = &gdt
+};
+
 
 struct gpt_entry {
   uint8_t partguid[16];
@@ -152,10 +194,12 @@ void kmain(void) {
     if (beginlba == 0) return;
     ulong kernelloc = (*(cache+8)) + beginlba;
     err = drives[0].read(0, kernelloc, 1, cache); // read the highlighted file, which should be the kernel
-    char kernelcache[512*(*(cache+74+8) - *(cache+74))];
+
+    asm volatile ("lgdt gdtptr");
+
     // blindly trust that its the kernel, and that it only makes up one range of sectors
     for (uint sectors=0;sectors < *(cache+74+8) - *(cache+74);sectors++)
-      drives[0].read(0, *(cache+74) + beginlba + sectors, 1, kernelcache+(sectors*512));
+      drives[0].read(0, *(cache+74) + beginlba + sectors, 1, ((void *) largestfree.base)+(sectors*512));
     
   }
   
