398e4102649f2c498983c5b8e266c28c6600cf62
wellsilver <wellsilver2016@gmail.com>
2024-11-01 20:17:29 -0400

    x

--- src/limineboot.c
+++ src/limineboot.c
@@ -37,45 +37,6 @@ static volatile LIMINE_REQUESTS_START_MARKER;
 __attribute__((used, section(".requests_end_marker")))
 static volatile LIMINE_REQUESTS_END_MARKER;
 
-// translated from src/boot.x86.S
-// a new gdt with all of memory listed as readable, writable, and executable
-/*
-; Access bits
-PRESENT        equ 1 << 7
-NOT_SYS        equ 1 << 4
-EXEC           equ 1 << 3
-DC             equ 1 << 2
-RW             equ 1 << 1
-ACCESSED       equ 1 << 0
- 
-; Flags bits
-GRAN_4K       equ 1 << 7
-SZ_32         equ 1 << 6
-LONG_MODE     equ 1 << 5
- 
-GDT64: ; 64 bit gdt
-  .Null: equ $ - GDT64
-    dq 0
-  .Code: equ $ - GDT64
-    dd 0xFFFF                                   ; Limit & Base (low, bits 0-15)
-    db 0                                        ; Base (mid, bits 16-23)
-    db PRESENT | NOT_SYS | EXEC | RW            ; Access
-    db GRAN_4K | LONG_MODE | 0xF                ; Flags & Limit (high, bits 16-19)
-    db 0                                        ; Base (high, bits 24-31)
-  .Data: equ $ - GDT64
-    dd 0xFFFF                                   ; Limit & Base (low, bits 0-15)
-    db 0                                        ; Base (mid, bits 16-23)
-    db PRESENT | NOT_SYS | RW                   ; Access
-    db GRAN_4K | SZ_32 | 0xF                    ; Flags & Limit (high, bits 16-19)
-    db 0                                        ; Base (high, bits 24-31)
-  .TSS: equ $ - GDT64
-    dd 0x00000068
-    dd 0x00CF8900
-  .Pointer:
-    dw $ - GDT64 - 1
-    dq GDT64
-*/
-
 struct gpt_entry {
   uint8_t partguid[16];
   uint8_t uniqueguid[16];
