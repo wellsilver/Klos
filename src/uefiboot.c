69a824a6cbb92f1c4d48e44fb2f2a7e0d57b0f9d
wellsilver <wellsilver2016@gmail.com>
2025-02-09 15:20:46 -0700

    list drives and partitions

--- src/uefiboot.c
+++ src/uefiboot.c
@@ -4,12 +4,19 @@
 #include <kfs/kfs.h>
 
 int main(int argc, char **argv) {
-  efi_guid_t blockguid = EFI_BLOCK_IO_PROTOCOL_GUID;
-  efi_block_io_t *blockio;
-  efi_status_t err = BS->LocateProtocol(&blockguid, NULL, (void**) &blockio);
+  efi_guid_t bioGuid = EFI_BLOCK_IO_PROTOCOL_GUID;
+  efi_block_io_t *blockiohandles[24];
+  uintn_t handle_size = sizeof(blockiohandles);
+  efi_status_t err = BS->LocateHandle(ByProtocol, &bioGuid, NULL, &handle_size, (efi_handle_t*) &blockiohandles);
   if (EFI_ERROR(err)) {printf("No blockio\n");sleep(2);return 0;};
-  
-  blockio->ReadBlocks(blockio, 0, 0, 0, 0);
+  handle_size /= sizeof(blockiohandles[0]);
 
+  for (unsigned int loop=0;loop<handle_size;loop++) {
+    if (!blockiohandles[loop]->Media->LogicalPartition) printf("Drive ");
+    else printf("Part  ");
+    printf("%p, %i\n", blockiohandles[loop], blockiohandles[loop]->Media->MediaId);
+  }
+  
+  
   while (1);
 }
