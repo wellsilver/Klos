86ae0dc0a8a76af69e22dc4935899b5cb25d961d
wellsilver <wellsilver2016@gmail.com>
2025-02-09 01:21:57 -0700

    check for blockio

--- src/uefiboot.c
+++ src/uefiboot.c
@@ -1,9 +1,15 @@
 // POSIX-UEFI
 
 #include <uefi.h>
-#include <../kfs/kfs.h>
+#include <kfs/kfs.h>
 
 int main(int argc, char **argv) {
-  printf("Hello world!\n");
+  efi_guid_t blockguid = EFI_BLOCK_IO_PROTOCOL_GUID;
+  efi_block_io_t *blockio;
+  efi_status_t err = BS->LocateProtocol(&blockguid, NULL, (void**) &blockio);
+  if (EFI_ERROR(err)) {printf("No blockio\n");sleep(2);return 0;};
+  
+  blockio->ReadBlocks(blockio, 0, 0, 0, 0);
+
   while (1);
 }
