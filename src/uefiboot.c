4e08928bce631406ff8cb1530ec456a62665124d
wellsilver <wellsilver2016@gmail.com>
2025-02-11 01:54:08 -0700

    show all blockio handles?

--- src/uefiboot.c
+++ src/uefiboot.c
@@ -8,15 +8,26 @@ int main(int argc, char **argv) {
   efi_block_io_t *blockiohandles[24];
   uintn_t handle_size = sizeof(blockiohandles);
   efi_status_t err = BS->LocateHandle(ByProtocol, &bioGuid, NULL, &handle_size, (efi_handle_t*) &blockiohandles);
-  if (EFI_ERROR(err)) {printf("No blockio\n");sleep(2);return 0;};
   handle_size /= sizeof(blockiohandles[0]);
+  if (EFI_ERROR(err) || handle_size == 0) {printf("No blockio\n");sleep(2);return 0;};
 
+  printf("Present - FLAG - POINTER          - MEDIA ID\n");
   for (unsigned int loop=0;loop<handle_size;loop++) {
-    if (!blockiohandles[loop]->Media->LogicalPartition) printf("Drive ");
-    else printf("Part  ");
-    printf("%p, %i\n", blockiohandles[loop], blockiohandles[loop]->Media->MediaId);
+    // Present
+    if (blockiohandles[loop]->Media->MediaPresent == 1) printf("Yes     - ");
+    else printf("No      - ");
+    // Flags
+    if (blockiohandles[loop]->Media->RemovableMedia == 1) putchar('R');
+    else putchar(' ');
+    if (blockiohandles[loop]->Media->LogicalPartition == 1) putchar('L');
+    else putchar(' ');
+    if (blockiohandles[loop]->Media->ReadOnly == 1) putchar('r');
+    else putchar(' ');
+    if (blockiohandles[loop]->Media->WriteCaching == 1) putchar('w');
+    else putchar(' ');
+
+    printf(" - %p - %i\n", blockiohandles[loop], blockiohandles[loop]->Media->MediaId);
   }
-  
   
   while (1);
 }
