21c8f5dc923178bdd41836e258511bc897f9ca2c
wellsilver <wellsilver2016@gmail.com>
2025-02-15 23:48:10 -0700

    findkfs function

--- src/uefiboot.c
+++ src/uefiboot.c
@@ -3,34 +3,27 @@
 #include <uefi.h>
 #include <kfs/kfs.h>
 
-int main(int argc, char **argv) {
+efi_block_io_t findkfs() {
   efi_guid_t bioGuid = EFI_BLOCK_IO_PROTOCOL_GUID;
   efi_handle_t blockiohandles[24];
   efi_block_io_t *blockio[24];
   uintn_t handle_size = sizeof(blockiohandles);
   efi_status_t err = BS->LocateHandle(ByProtocol, &bioGuid, NULL, &handle_size, &blockiohandles);
   handle_size /= sizeof(blockiohandles[0]);
-  if (EFI_ERROR(err) || handle_size == 0) {printf("No blockio\n");sleep(2);return 0;};
+  if (EFI_ERROR(err) || handle_size == 0) {printf("No blockio\n");exit(-1);};
 
-  printf("Present - FLAG - POINTER          - MEDIA ID\n");
   for (unsigned int loop=0;loop<handle_size;loop++) {
     err = BS->HandleProtocol(blockiohandles[loop], &bioGuid, &blockio[loop]);
-    if (EFI_ERROR(err)) {printf("Error blockio handle %i\n", loop); break;};
-    // Present
-    if (blockio[loop]->Media->MediaPresent == 1) printf("Yes     - ");
-    else printf("No      - ");
-    // Flags
-    if (blockio[loop]->Media->RemovableMedia == 1) putchar('R');
-    else putchar(' ');
-    if (blockio[loop]->Media->LogicalPartition == 1) putchar('L');
-    else putchar(' ');
-    if (blockio[loop]->Media->ReadOnly == 1) putchar('r');
-    else putchar(' ');
-    if (blockio[loop]->Media->WriteCaching == 1) putchar('w');
-    else putchar(' ');
-
-    printf(" - %p - %i\n", blockiohandles[loop], blockio[loop]->Media->MediaId);
+    if (EFI_ERROR(err)) {printf("Error blockio handle %i\n", loop);break;};
   }
-  
-  while (1);
+  // First search partitions, it seems like computers will only create logical partition/fake drive things for the boot drive
+  for (unsigned int loop=0;loop<handle_size;loop++) {
+    
+  }
+}
+
+int main(int argc, char **argv) {
+  efi_block_io_t kfs = findkfs();
+
+  while (1) sleep(1);
 }
