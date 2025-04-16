f86ea8da23e5e843bb7568774b2a9d6a3d8d3ba2
wellsilver <wellsilver2016@gmail.com>
2025-04-15 21:21:01 -0700

    meminit

--- src/kernel/memory/mem.c
+++ src/kernel/memory/mem.c
@@ -1,1 +1,11 @@
-// TODO
+#include <memory/mem.h>
+#include <int.h>
+
+void meminit(struct memregion *a, uint len) {
+  // First, looking for the largest block of free memory
+  struct memregion largest = a[0];
+  for (uint i=1;i<len;i++)
+    if (a[i].size > largest.size) largest = a[i];
+  
+  
+}
