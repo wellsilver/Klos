84237e0248dfaaa454f99e748a2f6ebea8773746
wellsilver <wellsilver2016@gmail.com>
2025-05-17 23:30:25 -0700

    fix dependencies

--- installtools.sh
+++ installtools.sh
@@ -1,5 +1,7 @@
 # installs everything required using apt
 apt -y update
-apt -y install nasm qemu-system-x86 make wget python3 parted lld clang
+# sometimes parted is not put in /usr/bin even though its usable on (as file) disk images....
+# if that occurs sudo ln -s /usr/sbin/parted /usr/bin/parted
+apt -y install nasm qemu-system-x86 make wget python3 parted mtools lld clang
 bash ./bintil_cross.sh
 bash ./gcc_cross.sh
