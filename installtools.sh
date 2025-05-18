79b1d38771928a8e4ab0e55dd703c749af9bd2b0
wellsilver <wellsilver2016@gmail.com>
2025-05-17 23:23:48 -0700

    add lld and clang for EFI

--- installtools.sh
+++ installtools.sh
@@ -1,5 +1,5 @@
 # installs everything required using apt
 apt -y update
-apt -y install nasm qemu-system-x86 make wget python3 parted
+apt -y install nasm qemu-system-x86 make wget python3 parted lld clang
 bash ./bintil_cross.sh
 bash ./gcc_cross.sh
