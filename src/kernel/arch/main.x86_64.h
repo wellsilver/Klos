b765cf2e84a50d63c2b58cfdba41e12825b4b11a
wellsilver <wellsilver2016@gmail.com>
2025-08-23 23:53:30 -0700

    fix warnings and header file

---
+++ src/kernel/arch/main.x86_64.h
@@ -0,0 +1,15 @@
+#ifndef arch_main_x86_64_h
+#define arch_main_x86_64_h
+/*
+The header file with the locations of the tss64 and idt64 setup by main.x86_64.s
+
+global entry
+global tss64
+global idt64
+*/
+
+extern void *tss64;
+extern void *idt64;
+
+
+#endif
