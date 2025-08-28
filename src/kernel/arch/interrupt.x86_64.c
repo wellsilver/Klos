42fa1116017ba2deb4da0352d9b7e85387598bc2
wellsilver <wellsilver2016@gmail.com>
2025-08-27 23:56:34 -0700

    idtsetgate

--- src/kernel/arch/interrupt.x86_64.c
+++ src/kernel/arch/interrupt.x86_64.c
@@ -1,15 +1,24 @@
 #include <int.h>
+#include <arch/main.x86_64.h>
 
-struct InterruptDescriptor64 {
-  uint16_t offset_1;        // offset bits 0..15
-  uint16_t selector;        // a code segment selector in GDT or LDT
-  uint8_t  ist;             // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
-  uint8_t  type_attributes; // gate type, dpl, and p fields
-  uint16_t offset_2;        // offset bits 16..31
-  uint32_t offset_3;        // offset bits 32..63
-  uint32_t zero;            // reserved
+struct InterruptDescriptor {
+  uint16_t offset_1;   // offset bits 0..15
+  uint16_t selector;   // a code segment selector in GDT or LDT
+  uint8_t  ist;        // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
+  uint8_t  attributes; // gate type, dpl, and p fields
+  uint16_t offset_2;   // offset bits 16..31
+  uint32_t offset_3;   // offset bits 32..63
+  uint32_t zero;       // reserved
 };
 
-void iaddgate() {
+void idtsetgate(int gate, void *ptr, uint8_t flags) {
+  struct InterruptDescriptor *desc = idt64;
 
+  desc[gate].offset_1 = (uint64_t) ptr & 0xFFFF;
+  desc[gate].selector = 0x8; // Code
+  desc[gate].ist      = 0;
+  desc[gate].attributes = flags;
+  desc[gate].offset_2 = ((uint64_t)ptr >> 16) & 0xFFFF;
+  desc[gate].offset_3 = ((uint64_t)ptr >> 32) & 0xFFFFFFFF;
+  desc[gate].zero = 0;
 }
